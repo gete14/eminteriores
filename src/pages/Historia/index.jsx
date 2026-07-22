40939173237640aa937398d83d3f9995e7b85597
GustavoRobs-11 <gustavo.silva510@sp.gov.br>
2026-07-21 21:27:29 -0300

    version 1.5

--- src/pages/Historia/index.jsx
+++ src/pages/Historia/index.jsx
@@ -1,7 +1,7 @@
 import Objetivo from "./Objetivo.jsx";
 import Status from "./Status";
 import Comecou from "./Comecou.jsx";
-import Hero from "./Hero.jsx";
+import Hero from "./hero.jsx";
 
 export default function Historia() {
   
